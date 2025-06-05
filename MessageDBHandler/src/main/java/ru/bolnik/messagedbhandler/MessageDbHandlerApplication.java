be42b50d03134ccf0aa2df8eb57230f1f0890eff
Dima Bolnik <dimabolnik@mail.ru>
2025-06-05 16:58:41 +0300

    Добавление MessageDbHandlerApplication, при приема и обработки сообщений

---
+++ MessageDBHandler/src/main/java/ru/bolnik/messagedbhandler/MessageDbHandlerApplication.java
@@ -0,0 +1,13 @@
+package ru.bolnik.messagedbhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageDbHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageDbHandlerApplication.class, args);
+    }
+
+}
