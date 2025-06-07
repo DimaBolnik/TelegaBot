acd8210a5c2fc8c12aae044606472d15f4587cbb
Dima Bolnik <dimabolnik@mail.ru>
2025-06-07 11:24:15 +0300

    Создание сущности Болт для БД.

---
+++ MessageDBHandler/src/main/java/ru/bolnik/messagedbhandler/entity/Bolt.java
@@ -0,0 +1,29 @@
+package ru.bolnik.messagedbhandler.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "bolts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Bolt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "gost", nullable = false, length = 50)
+    private String gost;
+
+    @Column(name = "size", nullable = false, length = 20)
+    private String size;
+
+    @Column(name = "length_mm", nullable = false)
+    private Integer length; // длина в миллиметрах
+
+    @Column(name = "weight_gram", nullable = false)
+    private Double weight; // вес одного болта в граммах
+}
