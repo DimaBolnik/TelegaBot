10d27b9b89ea6dcf0877c22890f1c2d418987b68
Dima Bolnik <dimabolnik@mail.ru>
2025-06-11 17:31:22 +0300

    Перенос методов в UserDialogService

--- Dispatcher/src/main/java/ru/bolnik/dispatcher/controller/TelegramMessageDispatcher.java
+++ Dispatcher/src/main/java/ru/bolnik/dispatcher/controller/TelegramMessageDispatcher.java
@@ -3,9 +3,8 @@ package ru.bolnik.dispatcher.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import ru.bolnik.dispatcher.service.KafkaUpdateService;
+import ru.bolnik.dispatcher.service.UserDialogService;
 
 //  Обрабатывает входящие обновления (Update) и может быть расширен для обработки разных типов команд или состояний.
 @Component
@@ -13,40 +12,26 @@ public class TelegramMessageDispatcher {
 
     private static final Logger logger = LoggerFactory.getLogger(TelegramBot.class);
 
-//  Храним ссылку на TelegramBot и использует его для отправки ответов
-    private TelegramBot telegramBot;
-    private final KafkaUpdateService kafkaUpdateService;
 
-    public TelegramMessageDispatcher(KafkaUpdateService kafkaUpdateService) {
-        this.kafkaUpdateService = kafkaUpdateService;
+    //  Храним ссылку на TelegramBot и использует его для отправки ответов
+    private TelegramBot telegramBot;
+
+    private final UserDialogService userDialogService;
+
+    public TelegramMessageDispatcher(UserDialogService userDialogService) {
+        this.userDialogService = userDialogService;
     }
+
 
     public void registerBot(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
     }
 
     public void processUpdate(Update update) {
-        if (isHasText(update)) {
-            String messageText = update.getMessage().getText();
-            Long chatId = update.getMessage().getChatId();
-            logger.info("Получено сообщение: {} от пользователя с chatId: {}", messageText, chatId);
-
-            // Отправляем в Kafka
-            kafkaUpdateService.sendUpdateToKafka(update);
-
-            responseBot(chatId);
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            userDialogService.processUpdate(update);
         }
+
     }
 
-    private void responseBot(Long chatId) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(chatId.toString());
-        sendMessage.setText("сообщение получено");
-        // Отправляем ответ пользователю
-        telegramBot.sendResponse(sendMessage);
-    }
-
-    private static boolean isHasText(Update update) {
-        return update.hasMessage() && update.getMessage().hasText();
-    }
 }
