0092216ac17b7747b3820458cbaade8cbfd814d2
Dima Bolnik <dimabolnik@mail.ru>
2025-06-15 17:24:22 +0300

    добавление новы состояний в DialogStateEnum.

--- Dispatcher/src/main/java/ru/bolnik/dispatcher/service/data/DialogStateEnum.java
+++ Dispatcher/src/main/java/ru/bolnik/dispatcher/service/data/DialogStateEnum.java
@@ -1,11 +1,15 @@
 package ru.bolnik.dispatcher.service.data;
 
-// состояние диалога с пользователем для болтов и гаек
+/* состояние диалога с пользователем для болтов и гаек,
+ пока так, потом организую как0нибудь по другому.*/
 public enum DialogStateEnum {
     START,
     WAIT_FOR_PRODUCT_TYPE,
     WAIT_BOLT_GOST,
     WAIT_BOLT_SIZE,
     WAIT_BOLT_LENGTH,
-    WAIT_BOLT_WEIGHT
+    WAIT_BOLT_WEIGHT,
+    WAIT_NUT_GOST,
+    WAIT_NUT_SIZE,
+    WAIT_NUT_WEIGHT
 }
