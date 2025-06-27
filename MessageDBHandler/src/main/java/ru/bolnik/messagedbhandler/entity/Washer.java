7189e08c03422d53e3c3a03dbc1ebded95376670
Dima Bolnik <dimabolnik@mail.ru>
2025-06-27 09:54:26 +0300

    Добавление сущности Шайбы(Washer).

---
+++ MessageDBHandler/src/main/java/ru/bolnik/messagedbhandler/entity/Washer.java
@@ -0,0 +1,28 @@
+package ru.bolnik.messagedbhandler.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+// класс для шайбы
+@Entity
+@Table(name = "washers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Washer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "gost", nullable = false, length = 50)
+    private String gost;
+
+    @Column(name = "size", nullable = false, length = 20)
+    private String size;
+
+    @Column(name = "weight_gram", nullable = false)
+    private Double weight; // вес одной шайбы в граммах
+}
+
