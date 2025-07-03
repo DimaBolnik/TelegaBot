ad66de0fe150430244468dd4aa6913e5ef508c28
Dima Bolnik <dimabolnik@mail.ru>
2025-07-03 18:00:17 +0300

    Создание класса BoltDataStore для хранения состояния длины болта.

--- Dispatcher/src/main/java/ru/bolnik/dispatcher/model/tempState/BoltDataStore.java
+++ Dispatcher/src/main/java/ru/bolnik/dispatcher/model/tempState/BoltDataStore.java
@@ -5,49 +5,20 @@ import java.util.Map;
 
 
 // временное хранение данных болта до завершения диалога, хранение состояния сразу нескольких пользователей
-public class BoltDataStore {
+public class BoltDataStore extends ProductDataStore {
 
-    private static final Map<Long, String> gosts = new HashMap<>();
-    private static final Map<Long, String> sizes = new HashMap<>();
     private static final Map<Long, Integer> lengths = new HashMap<>();
-    private static final Map<Long, Double> weights = new HashMap<>();
 
-    public static void setTempGost(Long chatId, String gost) {
-        gosts.put(chatId, gost);
-    }
-
-    public static void setTempSize(Long chatId, String size) {
-        sizes.put(chatId, size);
-    }
-
-    public static void setTempLength(Long chatId, int length) {
+    public static void setLength(Long chatId, int length) {
         lengths.put(chatId, length);
     }
 
-    public static void setTempWeight(Long chatId, double weight) {
-        weights.put(chatId, weight);
-    }
-
-    public static String getTempGost(Long chatId) {
-        return gosts.get(chatId);
-    }
-
-    public static String getTempSize(Long chatId) {
-        return sizes.get(chatId);
-    }
-
-    public static int getTempLength(Long chatId) {
+    public static int getLength(Long chatId) {
         return lengths.getOrDefault(chatId, 0);
     }
 
-    public static double getTempWeight(Long chatId) {
-        return weights.getOrDefault(chatId, 0.0);
-    }
-
     public static void clear(Long chatId) {
-        gosts.remove(chatId);
-        sizes.remove(chatId);
         lengths.remove(chatId);
-        weights.remove(chatId);
+        ProductDataStore.clear(chatId);
     }
 }
